c1dffb3c3970535f261bb523c1e72677579e16d4
ThirdScript <space@coder.com>
2022-07-06 01:15:01 +0430

    Basic breathing mode 478

--- prana.py
+++ prana.py
@@ -1,17 +1,28 @@
 from rich.progress import track
 import time
 
+def BreathCycle(b_in: int, b_hold: int, b_out: int, b_hold_out: int, b_round: int):
+    for rnd_number in range(1, b_round+1):
+        print(F"\n\tRound: {rnd_number}")
+        print("\t"+("#-"*30))
 
-for rnd in range(3):
-    for n in track(range(400), description="Breath IN [4 Sec]"):
-        time.sleep(0.01)
-        n = n+1
+        for n in track(range(b_in* 100), description=F"Breath IN [{b_in} Sec]"):
+            time.sleep(0.01)
+            n = n+1
 
-    for n in track(range(700), description="HOLD [7 Sec]"):
-        time.sleep(0.01)
-        n = n+1
+        for n in track(range(b_hold * 100 - 100), description=F"HOLD [{b_hold} Sec]"):
+            time.sleep(0.01)
+            n = n+1
 
-    for n in track(range(800), description="Breath Out [8 Sec]"):
-        time.sleep(0.01)
-        n = n+1
-        
+        for n in track(range(b_out * 100 - 100), description=F"Breath Out [{b_out} Sec]"):
+            time.sleep(0.01)
+            n = n+1
+
+        for n in track(range((b_hold_out+1) * 100 - 100), description=F"Hold [{b_out} Sec]"):
+            time.sleep(0.01)
+            n = n+1
+            
+
+basic_breath = (4, 7, 8, 1, 2)
+
+BreathCycle(*basic_breath)
